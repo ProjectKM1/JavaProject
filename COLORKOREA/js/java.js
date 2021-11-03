a9db6a5c56a45c80991a044a173ae9e8fde8fd32
seungjin98 <89952669+seungjin98@users.noreply.github.com>
2021-11-03 15:33:38 +0900

    Ver js

---
+++ COLORKOREA/js/java.js
@@ -0,0 +1,102 @@
+// 외국인 방문객 차트 조절
+const jan = document.querySelector("#jan")
+const feb = document.querySelector("#feb")
+const mar = document.querySelector("#mar")
+const apr = document.querySelector("#apr")
+const may = document.querySelector("#may")
+const jun = document.querySelector("#jun")
+const jul = document.querySelector("#jul")
+const aug = document.querySelector("#aug")
+const sep = document.querySelector("#sep")
+const oct = document.querySelector("#oct")
+const nov = document.querySelector("#nov")
+const dec = document.querySelector("#dec")
+
+const japen = document.querySelector("#japen")
+const usa = document.querySelector("#usa")
+const china = document.querySelector("#china")
+const kor = document.querySelector("#kor")
+
+jan.addEventListener('click', function(){
+    japen.className = 'charts__chart chart--p30 chart--blue'
+    usa.className = 'charts__chart chart--p60 chart--green'
+    china.className = 'charts__chart chart--p40 chart--red'
+    kor.className = 'charts__chart chart--p20 chart--yellow'
+})
+
+feb.addEventListener('click', function(){
+    japen.className = 'charts__chart chart--p30 chart--blue'
+    usa.className = 'charts__chart chart--p60 chart--green'
+    china.className = 'charts__chart chart--p40 chart--red'
+    kor.className = 'charts__chart chart--p20 chart--yellow'
+})
+
+mar.addEventListener('click', function(){
+    japen.className = 'charts__chart chart--p30 chart--blue'
+    usa.className = 'charts__chart chart--p60 chart--green'
+    china.className = 'charts__chart chart--p40 chart--red'
+    kor.className = 'charts__chart chart--p20 chart--yellow'
+})
+
+apr.addEventListener('click', function(){
+    japen.className = 'charts__chart chart--p30 chart--blue'
+    usa.className = 'charts__chart chart--p60 chart--green'
+    china.className = 'charts__chart chart--p40 chart--red'
+    kor.className = 'charts__chart chart--p20 chart--yellow'
+})
+
+may.addEventListener('click', function(){
+    japen.className = 'charts__chart chart--p30 chart--blue'
+    usa.className = 'charts__chart chart--p60 chart--green'
+    china.className = 'charts__chart chart--p40 chart--red'
+    kor.className = 'charts__chart chart--p20 chart--yellow'
+})
+
+jun.addEventListener('click', function(){
+    japen.className = 'charts__chart chart--p30 chart--blue'
+    usa.className = 'charts__chart chart--p60 chart--green'
+    china.className = 'charts__chart chart--p40 chart--red'
+    kor.className = 'charts__chart chart--p20 chart--yellow'
+})
+
+jul.addEventListener('click', function(){
+    japen.className = 'charts__chart chart--p30 chart--blue'
+    usa.className = 'charts__chart chart--p60 chart--green'
+    china.className = 'charts__chart chart--p40 chart--red'
+    kor.className = 'charts__chart chart--p20 chart--yellow'
+})
+
+aug.addEventListener('click', function(){
+    japen.className = 'charts__chart chart--p30 chart--blue'
+    usa.className = 'charts__chart chart--p60 chart--green'
+    china.className = 'charts__chart chart--p40 chart--red'
+    kor.className = 'charts__chart chart--p20 chart--yellow'
+})
+
+sep.addEventListener('click', function(){
+    japen.className = 'charts__chart chart--p30 chart--blue'
+    usa.className = 'charts__chart chart--p60 chart--green'
+    china.className = 'charts__chart chart--p40 chart--red'
+    kor.className = 'charts__chart chart--p20 chart--yellow'
+})
+
+oct.addEventListener('click', function(){
+    japen.className = 'charts__chart chart--p30 chart--blue'
+    usa.className = 'charts__chart chart--p60 chart--green'
+    china.className = 'charts__chart chart--p40 chart--red'
+    kor.className = 'charts__chart chart--p20 chart--yellow'
+})
+
+nov.addEventListener('click', function(){
+    japen.className = 'charts__chart chart--p30 chart--blue'
+    usa.className = 'charts__chart chart--p60 chart--green'
+    china.className = 'charts__chart chart--p40 chart--red'
+    kor.className = 'charts__chart chart--p20 chart--yellow'
+})
+
+dec.addEventListener('click', function(){
+    japen.className = 'charts__chart chart--p30 chart--blue'
+    usa.className = 'charts__chart chart--p60 chart--green'
+    china.className = 'charts__chart chart--p40 chart--red'
+    kor.className = 'charts__chart chart--p20 chart--yellow'
+})
